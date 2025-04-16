3db85c2ce635c39df370dc54d279d7e46c492a62
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-15 22:07:05 -0600

    rename hook_handlers/ directory to hooks/, specify type import

--- src/lib/contract_sizer.ts
+++ src/lib/contract_sizer.ts
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 import Table from 'cli-table3';
 import fs from 'fs';
 import { HardhatPluginError } from 'hardhat/plugins';
-import { HookContext } from 'hardhat/types/hooks';
+import type { HookContext } from 'hardhat/types/hooks';
 import path from 'path';
 import stripAnsi from 'strip-ansi';
 
