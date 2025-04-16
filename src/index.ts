5d72a85376b19bee2e99f10c0d274d56cc11246b
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-15 22:09:46 -0600

    move task definition to tasks/ directory

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import pkg from '../package.json';
+import taskSizeContracts from './tasks/size_contracts.js';
 import './type-extensions.js';
-import { globalOption, task } from 'hardhat/config';
+import { globalOption } from 'hardhat/config';
 import { ArgumentType } from 'hardhat/types/arguments';
 import type { HardhatPlugin } from 'hardhat/types/plugins';
 
 const plugin: HardhatPlugin = {
   id: pkg.name.split('/').pop()!,
   npmPackage: pkg.name!,
-  tasks: [
-    task('size-contracts')
-      .setDescription('Output the size of compiled contracts')
-      .setAction(import.meta.resolve('./actions/size_contracts.js'))
-      .addFlag({
-        name: 'noCompile',
-        description: "Don't compile before running this task",
-      })
-      .build(),
-  ],
+  tasks: [taskSizeContracts],
   hookHandlers: {
     config: import.meta.resolve('./hooks/config.js'),
     solidity: import.meta.resolve('./hooks/solidity.js'),
---
+++ src/tasks/size_contracts.ts
@@ -0,0 +1,10 @@
+import { task } from 'hardhat/config';
+
+export default task('size-contracts')
+  .setDescription('Output the size of compiled contracts')
+  .setAction(import.meta.resolve('../actions/size_contracts.js'))
+  .addFlag({
+    name: 'noCompile',
+    description: "Don't compile before running this task",
+  })
+  .build();
