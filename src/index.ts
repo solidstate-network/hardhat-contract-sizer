6833784acdc6b589fa4fa6025ea791ee6852c66d
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-10 00:47:06 -0600

    remove non-null assertion from pkg.name

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import { ArgumentType } from 'hardhat/types/arguments';
 import type { HardhatPlugin } from 'hardhat/types/plugins';
 
 const plugin: HardhatPlugin = {
-  id: pkg.name!,
-  npmPackage: pkg.name!,
+  id: pkg.name,
+  npmPackage: pkg.name,
   tasks: [taskSizeContracts, taskSizeContractsDiff],
   hookHandlers: {
     config: import.meta.resolve('./hooks/config.js'),
